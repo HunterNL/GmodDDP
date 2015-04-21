18fa2fc8a6fc3f55d683d80c9243ffb1f3547805
HunterNL <hunternl@users.noreply.github.com>
2015-04-22 01:33:48 +0200

    Large code changes Added DDPCollection

---
+++ collection.lua
@@ -0,0 +1,39 @@
+if CLIENT then return end
+
+print("Collections loaded")
+
+if not DDPCollection then
+	DDPCollection = {}
+	DDPCollection.__index = DDPCollection
+	setmetatable(DDPCollection,{__call = function(self,...)
+		return DDPCollection.Create(...)
+	end})
+end
+
+function DDPCollection.Create(ddp,name)
+	local self = setmetatable({},DDPCollection)
+	self.ddp = ddp
+	self.name = name
+	self.populated = false
+	self.data = {}
+
+	self.ddp:LinkCollection(self)
+
+	return self
+end
+
+function DDPCollection:Add(id,fields)
+	self.data[id]=fields
+end
+
+function DDPCollection:Change(id,fields,cleared)
+	self.data[data.id]=data.fields
+end
+
+function DDPCollection:Remove(id)
+	self.data[data.id]=nil
+end
+
+function DDPCollection:OnPopulated()
+	print("DB POPULATED")
+end
--- ddp.lua
+++ ddp.lua
@@ -10,16 +10,19 @@ if not DDP then
 	DDP.__index = DDP
 end
 
-DDP.methodId = 1
+DDP.uid = 1
 
 function DDP.Create(url,port)
 	local self = setmetatable({},DDP)
 
-	self.socket = WS.Create(url.."/websocket",port)
+	self.messageHandlers = {}
+
+	self.socket = WS(url.."/websocket",port)
 	self.socket:Connect()
 	self.state="CONNECTING"
 
 	self.collections = {}
+	self.collectionEventQueue = {}
 	self.session = ""
 
 	self.socket:SetCallbackReceive(function(data)
@@ -27,47 +30,132 @@ function DDP.Create(url,port)
 	end)
 
 	self.socket:SetCallbackConnected(function()
-		self:OnOpen()
+		self:OnWSOpen()
 	end)
 
 	self.socket:SetCallbackClose(function(byclient)
-		self:OnClose(byclient)
+		self:OnWSClose(byclient)
 	end)
 
 	return self
 end
 
-function DDP:OnOpen()
-	self.state="OPEN"
+function DDP:OnWSOpen() --fires when websocket is connected
 	self:Connect()
 end
 
-function DDP:OnMessage(data)
-	print("Incoming message "..data)
-	local data = util.JSONToTable(data)
+function DDP:OnConnected() --fires when ddp is connected
 
-	if(data.msg=="ping") then
-		self:Write("{\"msg\":\"pong\"}")
+end
+
+function DDP:AddEventToQueue(event)
+	local collectionqueue = self.collectionEventQueue[event.collection]
+
+	if(!collectionEventQueue) then
+		collectionqueue = {}
+		self.collectionEventQueue[event.collection] = collectionEventQueue
 	end
+	table.insert(collectionqueue,event)
+end
 
-	if(data.msg=="added") then
-		local collection = data.collection
-		local id = data.id
-		local fields = data.fields
-
-		if(self.collections[collection]!=nil) then
-			self.collections[collection][id] = fields
-		else
-			self.collections[collection]={id=fields}
+function DDP:ProcessEventQueue(collection)
+	if(self.collectionEventQueue[collection.name]!=nil) then
+		local queue = self.collectionEventQueue[collection.name]
+		for k,v in ipairs(queue) do
+			self:OnMessage(v)
 		end
+		queue = nil
+		print(self.collectionEventQueue[collection.name])
 	end
+	collection.populated = true
+	collection:OnPopulated()
+end
 
-	if(data.msg=="connected") then
-		self.session=data.session
+function DDP:LinkCollection(collection)
+	if(self.collections[collection.name]!=nil) then
+		Error("Colletion with this name already linked")
+	else
+		self.collections[collection.name]=collection
+		self:ProcessEventQueue(collection)
 	end
 end
 
-function DDP:OnClose()
+function DDP:OnMessage(message)
+	print("Incoming message "..message)
+	local event = util.JSONToTable(message)
+	local collectionName = event.collection
+	local collection = self.collections[collectionName]
+	local eventType = event.msg
+
+	if(eventType=="added") then
+		if(collection) then
+			self:DataAdded(event)
+			return
+		end
+	end
+
+	if(eventType=="changed") then
+		if(collection) then
+			self:DataChanged(event)
+			return
+		end
+	end
+
+	if(eventType=="removed") then
+		if(collection) then
+			self:DataRemoved(event)
+			return
+		end
+	end
+
+	if(eventType=="connected") then
+		self.state="OPEN"
+		self.session=event.session
+
+		self:OnConnected()
+		return
+	end
+
+	if(event.msg=="ping") then
+		self:Write("{\"msg\":\"pong\"}")
+		return
+	end
+
+	if(eventType =="added" or eventType == "changed" or eventType=="remove") then
+		self:AddEventToQueue(event) --If unhandled, add to que
+		return
+	end
+
+	if(event.server_id) then
+		return --Ignore this eventType, is ment for outdated meteor clients
+	end
+
+	print("Unhandled DDP message "..(eventType or "NONE"))
+	PrintTable(event)
+end
+
+function DDP:DataAdded(event)
+	local collection = self.collections[event.collection]
+	if(collection) then
+		collection:Add(event.id,event.fields or nil)
+	end
+end
+
+function DDP:DataChanged(event)
+	local collection = self.collections[event.collection]
+	if(collection) then
+		collection:Change(event.id,event.fields or nil,event.cleared or nil)
+	end
+end
+
+function DDP:DataRemoved(event)
+	local collection = self.collections[event.collection]
+	if(collection) then
+		collection:remove(event.id)
+	end
+end
+
+function DDP:OnWSClose()
 
 end
 
@@ -79,11 +167,6 @@ function DDP:Write(data)
 	if (type(data) == "table") then
 		data = util.TableToJSON(data)
 	end
-	--[[local msg = util.TableToJSON({
-		msg = "connect",
-		version =  "1",
-		support = {1}
-	})--]]
 
 	print("Outdoing message"..data)
 	self.socket:Send(data)
@@ -93,34 +176,45 @@ function DDP:PrintCollections()
 	PrintTable(self.collections)
 end
 
-function DDP:Call(name,params)
-	local pstring --params string
+local function arrayToJSON(array)
+	local rstring --aRray string
 
-	if params == nil then
-		pstring = "[]"
+	if (array == nil) then
+		rstring = "[]"
 	else
-		pstring= "["
-		for k,v in pairs(params) do
-			pstring = pstring.."\""..v.."\""
+		rstring= "["
+		for k,v in ipairs(params) do
+			rstring = rstring.."\""..v.."\""
 
 			if(k!=#params) then
-				pstring = pstring..","
+				rstring = rstring..","
 			end
 		end
-		pstring = pstring.."]"
+		rstring = rstring.."]"
 	end
 
+	return rstring
+end
 
-
-	local msg = "{\"msg\":\"method\",\"method\":\""..name.."\",\"id\":\""..self:getMethodId().."\", \"params\" :"..pstring.."}"
+function DDP:Call(name,params,callback) --todo implement callback
+	local msg = "{\"msg\":\"method\",\"method\":\""..name.."\",\"id\":\""..self:getUid().."\", \"params\" :"..arrayToJSON(params).."}"
 	self:Write(msg)
 end
 
-function DDP:getMethodId()
-	local id = DDP.methodId
-	DDP.methodId = DDP.methodId + 1
+function DDP:Subscribe(collectionName,params)
+	local subid = self:getUid()
+	local msg = "{\"msg\":\"sub\",\"id\":\""..self:getUid().."\",\"name\":\""..collectionName.."\",\"params\":"..arrayToJSON(params).."}"
+	self:Write(msg)
+	return subid
+end
+
+function DDP:getUid()
+	local id = DDP.uid
+	DDP.uid = DDP.uid + 1
 	return id
 end
+
+
 
 function DDP:Connect()
 	local session
